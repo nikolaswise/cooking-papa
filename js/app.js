ac5ffd2587f3fcb5065cdeeca76fd83db07453ae
Nik Wise <nwise@Niks-MacBook-Pro.local>
2019-01-14 14:29:03 -0800

    cache views and popstate navigation

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const navigate = (e) => {
   let pathname = e.target.pathname
   let html = sessionStorage.getItem(pathname)
   let node = document.querySelector('.region-main')
+  window.history.pushState(null, null, pathname);
   node.innerHTML = html
 }
 
@@ -20,6 +21,12 @@ const render = (pathname, json, template) => {
 }
 
 const prefetch = (e) => {
+  let cache = sessionStorage.getItem(e.target.pathname)
+
+  if (cache) {
+    return
+  }
+
   let pathname = e.target.pathname.split('/')
   let template = pathname[1]
   let route = pathname[2]
@@ -32,6 +39,19 @@ const prefetch = (e) => {
   request()
 }
 
+const popstate = (e) => {
+  console.debug('popstate:', e)
+  console.debug(document.location.pathname)
+  let html = sessionStorage.getItem(document.location.pathname)
+  let node = document.querySelector('.region-main')
+  node.innerHTML = html
+}
+
+const cache = () => {
+  let node = document.querySelector('.region-main')
+  sessionStorage.setItem(document.location.pathname, node.innerHTML)
+}
+
 const handler = (link) => {
   link.addEventListener('mouseover', prefetch)
   link.addEventListener('click', navigate)
@@ -39,3 +59,7 @@ const handler = (link) => {
 
 let links = Array(...document.querySelectorAll('a'))
 links.forEach(link => handler(link))
+
+cache()
+
+window.onpopstate = popstate
